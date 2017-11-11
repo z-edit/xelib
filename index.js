239b45083ea585ee7c744cddf26753dae53c8fe2
Mator <why101@msn.com>
2017-11-10 20:56:24 -0800

    added javascript wrapper library from zEdit

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-const xelib = require('bindings')('xelib');
-module.exports = xelib;
+const lib = require('bindings')('xelib');
+const xelib = {};
+const helpers = require('./lib/helpers')(lib, xelib);
+
+require('./lib/meta')(lib, xelib, helpers);
+require('./lib/messages')(lib, xelib, helpers);
+require('./lib/setup')(lib, xelib, helpers);
+require('./lib/files')(lib, xelib, helpers);
+require('./lib/masters')(lib, xelib, helpers);
+require('./lib/elements')(lib, xelib, helpers);
+require('./lib/errors')(lib, xelib, helpers);
+require('./lib/elementValues')(lib, xelib, helpers);
+require('./lib/serialization')(lib, xelib, helpers);
+require('./lib/records')(lib, xelib, helpers);
+require('./lib/groups')(lib, xelib, helpers);
+require('./lib/filter')(lib, xelib, helpers);
+require('./lib/fileValues')(lib, xelib, helpers);
+require('./lib/recordValues')(lib, xelib, helpers);
+require('./lib/common')(lib, xelib, helpers);
+require('./lib/utils')(lib, xelib, helpers);
+
+module.exports = {
+    helpers: helpers,
+    lib: lib,
+    wrapper: xelib
+};
